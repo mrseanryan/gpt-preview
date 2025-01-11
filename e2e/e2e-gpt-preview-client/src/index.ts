5da18f5b3e8436ea1c05b2adb6bec682dbe25554
Sean Ryan <str_ie@yahoo.co.uk>
2025-01-11 11:58:23 +0100

    Up e2e

--- e2e/e2e-gpt-preview-client/src/index.ts
+++ e2e/e2e-gpt-preview-client/src/index.ts
@@ -1,7 +1,15 @@
-import { OutputFormat, summarizeText } from "gpt-preview";
+import { OutputFormat, Platform, summarizeText } from "gpt-preview";
 
-const config = {
-  platform: "AwsBedrock",
+const originalText = `
+Generated 3 function calls
+[Agent: Creature Creator] AddCreature( creature_name=sheep, icon_name=sheep-icon, land_type=prairie, age=1 )
+[Agent: Plant Creator] AddPlant( plant_name=grass, icon_name=grass-icon, land_type=prairie )
+[Agent: Relationship Creator] AddCreatureRelationship( from_name=sheep, to_name=grass, relationship_name=eats )
+`;
+
+console.log("Testing via AWS Bedrock");
+const configAwsBedrock = {
+  platform: Platform.AwsBedrock,
   isDebug: false,
   modelId: "eu.anthropic.claude-3-5-sonnet-20240620-v1:0",
   maxTokens: 2048,
@@ -10,13 +18,23 @@ const config = {
   awsRegion: "eu-west-1",
 };
 
-const summary = summarizeText(
-  `
-Generated 3 function calls
-[Agent: Creature Creator] AddCreature( creature_name=sheep, icon_name=sheep-icon, land_type=prairie, age=1 )
-[Agent: Plant Creator] AddPlant( plant_name=grass, icon_name=grass-icon, land_type=prairie )
-[Agent: Relationship Creator] AddCreatureRelationship( from_name=sheep, to_name=grass, relationship_name=eats )
-`,
+const summaryAws = await summarizeText(
+  originalText,
   OutputFormat.JSON,
-  config
+  configAwsBedrock
+);
+
+console.log("Testing via Open AI");
+const configOpenAi = {
+  platform: Platform.OpenAI,
+  isDebug: false,
+  modelId: "gpt-4o-mini",
+  maxTokens: 2048,
+  temperature: 0.7,
+  top_p: 0.9,
+};
+const summaryOpenAi = await summarizeText(
+  originalText,
+  OutputFormat.JSON,
+  configAwsBedrock
 );
